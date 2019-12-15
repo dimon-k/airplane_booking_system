f7ccc5cbaf6bd8fe90d1c030fe1d8ad5a1f83f15
Dmitriy Kanununnikov <dmitriy.kanunnikov@gmail.com>
2019-12-15 17:21:08 +0100

    Configure 'spec_helper' for testing API

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,18 @@
 Dir['./lib/*.rb'].each { |file| require file }
+
+require 'rack/test'
+require 'rspec'
+
+ENV['RACK_ENV'] = 'test'
+
+require './application.rb'
+
+module RSpecMixin
+  include Rack::Test::Methods
+
+  def app()
+    Sinatra::Application
+  end
+end
+
+RSpec.configure { |config| config.include RSpecMixin }
