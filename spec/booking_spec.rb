57f9fa1973058d5c81fcef62ecd573b68e6153d9
Dmitriy Kanununnikov <dmitriy.kanunnikov@gmail.com>
2019-12-15 14:52:53 +0100

    Refactor existing rspecs

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -5,42 +5,29 @@ describe Booking do
                              sits_count: 156,
                              rows: 26,
                              row_arrangement: 'ABC_DEF') }
+  let(:booking) { described_class.new(plane) }
+  let(:book) { ->(name, seats) { booking.book(name, seats) } }
 
-  describe '#book' do
-    let(:booking) { described_class.new(plane) }
-    let(:book) { ->(name, seats) { booking.book(name, seats) } }
-
-    describe 'with 6 seats reservation in total' do
-      it 'returns A1, B1, A2, B2 reserved sets' do
-        expect(book['Marco', 4]).to eq(['A1', 'B1', 'A2', 'B2'])
-      end
-
-      it 'returns E1, F1 reserved sets' do
-        expect(book['Gerard', 2]).to eq(['E1', 'F1'])
+  context '#book' do
+    context 'with 6 seats reservation in total' do
+      it 'returns reserved seats' do
+        expect(book['Marco', 4]).to match_array(['A1', 'B1', 'A2', 'B2'])
+        expect(book['Gerard', 2]).to match_array(['E1', 'F1'])
       end
     end
 
-    describe 'with 8 seats reservation in total' do
-      it 'returns A1, B1 reserved sets' do
-        expect(book['Iosu', 2]).to eq(['A1', 'B1'])
-      end
-
-      it 'returns D1, E1, F1, E2, F2 reserved sets' do
-        expect(book['Oriol', 5]).to eq(['D1', 'E1', 'F1', 'E2', 'F2'])
-      end
-
-      it 'returns A2, B2 reserved sets' do
-        expect(book['David', 2]).to eq(['A2', 'B2'])
+    context 'with 8 seats reservation in total' do
+      it 'returns reserved seats' do
+        expect(book['Iosu', 2]).to match_array(['A1', 'B1'])
+        expect(book['Oriol', 5]).to match_array(['D1', 'E1', 'F1', 'E2', 'F2'])
+        expect(book['David', 2]).to match_array(['A2', 'B2'])
       end
     end
 
-    describe 'with 4 seats reservation in total' do
-      it 'returns A1, B1 reserved sets' do
-        expect(book['Iosu', 2]).to eq(['A1', 'B1'])
-      end
-
-      it 'returns E1, F1 reserved sets' do
-        expect(book['Gerard', 2]).to eq(['E1', 'F1'])
+    context 'with 4 seats reservation in total' do
+      it 'returns reserved seats' do
+        expect(book['Iosu', 2]).to match_array(['A1', 'B1'])
+        expect(book['Gerard', 2]).to match_array(['E1', 'F1'])
       end
     end
   end
