b7bd7420e86e3166e76f30ddf2fc6eeb8d8276ce
Dmitriy Kanununnikov <dmitriy.kanunnikov@gmail.com>
2019-12-16 16:57:12 +0100

    Final Booking class refactor

--- lib/booking.rb
+++ lib/booking.rb
@@ -3,24 +3,24 @@ class Booking
     @plane = plane
     @arrangement = plane.arrangement
     @list_of_all_seats = set_list_of_all_seats
-    @reservations = []
+    @all_reservations = []
   end
 
   def book(name, seats_amount)
     raise 'Reservation amount should be in range of 1 to 8 people' unless seats_amount.between?(1, 8)
 
-    more_than_three_seats(seats_amount, name) ||
-      assign_rest_of_seats(seats_amount, name) ||
-      random_assign(amount, name)
+    comfortable_allocation(seats_amount, name) ||
+      aisle_allocation(seats_amount, name) ||
+        random_allocation(amount, name)
   end
 
   def show
-    reservations
+    all_reservations
   end
 
   private
 
-  attr_reader :plane, :arrangement, :list_of_all_seats, :reservations
+  attr_reader :plane, :arrangement, :list_of_all_seats, :all_reservations
 
   def set_list_of_all_seats
     all_seats_holder = {} 
@@ -34,75 +34,60 @@ class Booking
     all_seats_holder
   end
 
-  def more_than_three_seats(amount, name)
-    max_in_row, full_rows, and_seats = if amount <= 3
-                                         [amount, 1, 0]
-                                       elsif amount % 2 == 1
-                                         [3, (amount / 3), (amount % 3)]
-                                       elsif amount % 3 == 1
-                                         [2, (amount / 2), (amount % 2)]
-                                       else
-                                         [3, (amount / 3), (amount % 3)]
-                                       end
+  def row_and_seats_params(amount)
+    return [amount, 1, 0] if amount <= 3
+    return [3, (amount / 3), (amount % 3)] if amount % 2 == 1
+    return [2, (amount / 2), (amount % 2)] if amount % 3 == 1
+    [3, (amount / 3), (amount % 3)]
+  end
+
+  def comfortable_allocation(amount, name)
+    max_in_row, full_rows, and_seats = row_and_seats_params(amount)
 
     draft_seats_holder = arrangement.each_with_index.map { |_v, i| arrangement[i...i+max_in_row] }
     seats_holder = draft_seats_holder.reject { |seats| seats.count < max_in_row || seats.any? { |seat| seat.eql?('_') } }
     matrix_seats_holder = []
     1.upto(plane.rows).each do |number|
       seats_holder.each do |seats|
-        @temp_seats_holder = []
+        temp_seats_holder = []
         (0...full_rows).each do |row_number|
-          @temp_seats_holder << seats.map { |seat| "#{seat}#{number + row_number}" }
+          temp_seats_holder << seats.map { |seat| "#{seat}#{number + row_number}" }
           unless number + row_number > plane.rows
-            @temp_seats_holder << seats[0...and_seats].map { |seat| "#{seat}#{number + row_number + 1}" }
+            temp_seats_holder << seats[0...and_seats].map { |seat| "#{seat}#{number + row_number + 1}" }
           end
         end
-        matrix_seats_holder << @temp_seats_holder.flatten.uniq
+        matrix_seats_holder << temp_seats_holder.flatten.uniq
       end
     end
-    matrix_seats_holder = matrix_seats_holder.reject { |seats| seats.count < amount }
-    matrix_seats_holder.each do |seats|
-      if seats.all? { |seat| list_of_all_seats[seat] }
-        seats.each { |seat| list_of_all_seats[seat] = false }
-        @selected_seats = seats
-        reservations << { name => seats }
-        break
-      end
-    end
-    @selected_seats
+    matrix_seats_holder = reject_incomplete_rows(matrix_seats_holder, amount)
+    assign_seats(matrix_seats_holder, name)
   end
 
-  def assign_rest_of_seats(amount, name)
-    aisle_seats = plane.row_arrangement.split('_').each_with_index.map do |seat, index|
-                    index % 2 == 0 ? seat[-1] : seat[0]
-                  end
+  def aisle_allocation(amount, name)
+    aisle_seats = plane.row_arrangement.split('_').each_with_index.map { |seat, index| index.even? ? seats : seats.reverse }
     available_seats = list_of_all_seats.select { |key, value| value && key.start_with?(*aisle_seats) }.keys
     matrix_seats_holder = available_seats.each_with_index.map { |_v, i| available_seats[i...i+amount] }
-    
-    matrix_seats_holder = matrix_seats_holder.reject { |seats| seats.count < amount }
-    matrix_seats_holder.each do |seats|
-      if seats.all? { |seat| list_of_all_seats[seat] }
-        seats.each { |seat| list_of_all_seats[seat] = false }
-        @selected_seats = seats
-        reservations << { name => seats }
-        break
-      end
-    end
-    @selected_seats
+    matrix_seats_holder = reject_incomplete_rows(matrix_seats_holder, amount)
+    assign_seats(matrix_seats_holder, name)
   end
 
-  def random_assign(amount, name)
+  def reject_incomplete_rows(matrix_seats_holder, amount)
+    matrix_seats_holder.reject { |seats| seats.count < amount }
+  end
+
+  def random_allocation(amount, name)
     available_seats = list_of_all_seats.select { |key, value| value }.keys
     matrix_seats_holder = [available_seats[0...amount]]
-    matrix_seats_holder.each do |seats|
-      if seats.all? { |seat| list_of_all_seats[seat] }
-        seats.each { |seat| list_of_all_seats[seat] = false }
-        @selected_seats = seats
-        reservations << { name => seats }
-        break
-      end
-    end
-    @selected_seats
+    assign_seats(matrix_seats_holder, name)
   end
 
+  def assign_seats(matrix_seats, name)
+    matrix_seats.each do |seats|
+      next unless seats.all? { |seat| list_of_all_seats[seat] }
+
+      seats.each { |seat| list_of_all_seats[seat] = false }
+      all_reservations << { name => seats }
+      return seats
+    end
+  end
 end
